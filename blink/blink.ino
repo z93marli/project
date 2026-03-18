70a142621df30a64a6ce18b8c1a135a102665989
z93marli <66039917+z93marli@users.noreply.github.com>
2026-03-18 20:42:41 +0100

    Update blink.ino with 7 segment support

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,13 +1,21 @@
-#define internal_led 13
+#include <TM1637Display.h>
 
+#define internal_led 13
+#define CLK 4
+#define DIO 3
+
+TM1637Display display(CLK, DIO);
 
 void setup ()
 {
-   pinMode(internal_led,OUTPUT);  
+   pinMode(internal_led,OUTPUT); 
+   display.setBrightness(3); // Sets the brightness level to 3
 }
 
 void loop()
 {
+   data[0]= display.encodeDigit(15); // This will display F___ on the display [0b01110001 = F]
+	display.setSegments(data);
    digitalWrite(internal_led,HIGH);
    delay(50);
    digitalWrite(internal_led,LOW);
